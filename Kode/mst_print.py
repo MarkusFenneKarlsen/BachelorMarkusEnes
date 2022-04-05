d2d1b6271a42bb09cc5eacf1575c49e2b438460c
EnesOk2000 <69807738+EnesOk2000@users.noreply.github.com>
2022-04-05 13:08:16 +0200

    Update mst_print.py

---
+++ Kode/mst_print.py
@@ -0,0 +1,14 @@
+import numpy as np;
+from sample_graph import SampleGraph;
+
+def mst_print(MST):
+    print("Printing the MST with the vertices and edges:")
+    TotalWT = 0
+    for i in range(MST.Nodes-1):
+        mst = MST[i][:]
+        u = mst[0]
+        v = mst[1]
+        wt = mst[2]
+        TotalWT += wt
+        print(f"Edge-{i}: {MST.Nodes[u]}-{MST.Nodes[v]} Wt:{wt}")
+    print(f"Total Weight: {TotalWT}")
